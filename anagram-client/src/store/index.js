2d82e39b2174f397ad474db77a1188896fae741f
VenansiusRendy <lierendysetiawan3232@gmail.com>
2021-06-10 23:15:28 +0800

    Added create room in lobby, and added room state in store, pass prop from lobby to room

--- anagram-client/src/store/index.js
+++ anagram-client/src/store/index.js
@@ -7,15 +7,23 @@ Vue.use(Vuex);
 export default new Vuex.Store({
 	state: {
 		playername: "",
+		roomNumber: "",
+		rooms: [],
 	},
 	mutations: {
 		SET_PLAYERNAME(state, payload) {
 			state.playername = payload;
 		},
+		SET_ROOMS(state, payload) {
+			state.rooms = payload;
+		},
 	},
 	actions: {
 		setPlayerName(context, payload) {
 			context.commit("SET_PLAYERNAME", payload);
+		},
+		setRooms(context, payload) {
+			context.commit("SET_ROOMS", payload);
 		},
 	},
 	modules: {},
