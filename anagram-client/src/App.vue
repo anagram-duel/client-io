89d97397571dbd0d85f757c5ffb5b14eeb218775
VenansiusRendy <lierendysetiawan3232@gmail.com>
2021-06-11 11:49:42 +0800

    Add audio file in assets and initiate audio in app.vue

--- anagram-client/src/App.vue
+++ anagram-client/src/App.vue
@@ -5,18 +5,21 @@
 </template>
 
 <script>
+const audio = new Audio(require("./assets/background.mp3"));
 export default {
   name: "App",
-  mounted() {},
+  mounted() {
+    audio.play();
+  },
 };
 </script>
 
 <style>
 * {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: 'Baloo Tammudu 2', cursive;
-	color: #90b4dc;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Baloo Tammudu 2", cursive;
+  color: #90b4dc;
 }
 </style>
