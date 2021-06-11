cd003528637754e061a8bace75f4ca4773ba31c8
VenansiusRendy <lierendysetiawan3232@gmail.com>
2021-06-11 16:38:57 +0800

    Change src in index.html public and socket.js config to production

--- anagram-client/src/config/socket.js
+++ anagram-client/src/config/socket.js
@@ -1,4 +1,4 @@
-// const socket = io.connect("https://anagram-duel.herokuapp.com");
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("https://anagram-duel.herokuapp.com");
+// const socket = io.connect("http://localhost:3000");
 
 module.exports = socket;
